0b8b4dd5bbd749ef990bf59ae48141a339741538
kogisal <kogisal099@gmail.com>
2025-05-23 20:08:55 +0900

    sss

--- Assets/Script/Player/Player_Move.cs
+++ Assets/Script/Player/Player_Move.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
-    float inputX;
-    float inputY;
-
     public float moveSpeed = 5f;
+    private Rigidbody2D rb;
 
-    Rigidbody2D rb;
+    public float moveInputX;
+    public float moveInputY;
 
+    bool isFacingRightX;
+    bool isFacingRightY;
+
+    public Transform attackPos;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,30 +19,46 @@ public class Player_Move : MonoBehaviour
 
     void Update()
     {
-        MoveX();
-        MoveY();
-    }
-
-    private void MoveY()
-    {
-        inputY = 0;
+        moveInputX = 0f;
+        moveInputY = 0f;
+        //MoveInput();
+        // W, S 키로 이동 처리
         if (Input.GetKey(KeyCode.W))
-            inputY = 1;
-        if (Input.GetKey(KeyCode.S))
-            inputY = -1;
-    }
+        {
+            moveInputY = 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            moveInputY = -1f;
+        }
 
-    private void MoveX()
-    {
-        inputX = 0;
+        // A, D 키로 이동 처리
         if (Input.GetKey(KeyCode.A))
-            inputX = -1;
-        if (Input.GetKey(KeyCode.D))
-            inputX = 1;
+        {
+            moveInputX = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveInputX = 1f;
+        }
+        Flip();
+
+        Vector3 movement = new Vector3(moveInputX, moveInputY, 0) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
     }
 
-    private void FixedUpdate()
+    private void Flip()
     {
-        rb.velocity = new Vector2(inputX, inputY) * moveSpeed;
+        if (isFacingRightX && moveInputX < 0f || !isFacingRightX && moveInputX > 0f)
+        {
+            isFacingRightX = !isFacingRightX;
+            attackPos.localPosition = new Vector3(isFacingRightX ? 1f : -1f, 0f, 0f);
+        }
+        if ((isFacingRightY && moveInputY < 0f) || (!isFacingRightY && moveInputY > 0f))
+        {
+            isFacingRightY = !isFacingRightY;
+            attackPos.localPosition = new Vector3(0f, isFacingRightY ? 1f : -1f, 0f);
+        }
     }
+
 }
